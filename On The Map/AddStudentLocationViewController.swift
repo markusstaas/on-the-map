c00532f53f3d50d23fad04df4646e9e8b47aa028
Markus Staas <markus.staas@me.com>
2017-07-27 15:39:54 +0700

    -added new location screen -added confirm new location screen

---
+++ On The Map/AddStudentLocationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddStudentLocationViewController.swift
+//  On The Map
+//
+//  Created by Markus Staas on 7/27/17.
+//  Copyright © 2017 Markus Staas. All rights reserved.
+//
+
+import UIKit
+
+class AddStudentLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backButt(segue:UIStoryboardSegue){
+        let _ = self.navigationController?.popViewController(animated: true)    }
+
+
+
+
+}
